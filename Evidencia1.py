288087f62bb199ec53ba582d1aaf1c1007e9de59
Alumno-2026415 <143697133+Alumno-2026415@users.noreply.github.com>
2023-09-02 15:30:28 -0600

    Update Evidencia1.py
    Le agregué más líneas al código

--- Evidencia1.py
+++ Evidencia1.py
@@ -13,13 +13,33 @@ while True:
     opcion = input("Ingrese una opción:\n")
     
     if opcion == "1":
-        
         folios_contador += 1
-        
-        cliente = input("Nombre del cliente:\n")
-        
-        fecha_ingresada_str = input("Fecha de la nota (dd-mm-aaaa):\n")
-        fecha_ingresada = datetime.datetime.strftime(fecha_ingresada_str, "%d-%m-%Y")
+        while True:
+            nombre_cliente = input("Nombre del cliente:\n")
+            if not nombre_cliente.strip():
+                print("El folio no puede estar en blanco. Intente de nuevo.")
+                continue
+            else:
+                break
+
+        while True:
+            fecha_actual = datetime.datetime.now()
+            fecha_ingresada_str = input("Fecha de la nota (dd-mm-aaaa):\n")
+            if not fecha_ingresada_str:
+                fecha_ingresada_str = fecha_actual.strftime('%d-%m-%Y')
+                if not re.match(patron_fecha, fecha_ingresada_str):
+                    print("Formato de fecha incorrecto. Debe ser dd-mm-aaaa.")
+                continue
+            try:
+                fecha_ingresada = datetime.datetime.strftime(fecha_ingresada_str, "%d-%m-%Y")
+            except Exception:
+                print("La fecha no es válida. Intente de nuevo.")
+                continue
+            if fecha_ingresada > fecha_actual:
+                print("La fecha no puede ser posterior a la fecha actual del sistema.")
+                continue
+            else:
+                break
         
         nota = {
             "folio": folios_contador,
@@ -31,15 +51,39 @@ while True:
         }
         
         while True:
-            
             nombre_servicio = input("Nombre del servicio: ")
-            costo_servicio = input("Costo del servicio: ")
+            if not nombre_servicio.strip():
+                print("El nombre del servicio no puede estar en blanco. Intente de nuevo")
+                continue
+
+        while True:
+            costo_servicio_str = input("Costo del servicio: ")
+            if not costo_servicio_str.strip():
+                print("El costo no puede estar en blanco. Intente de nuevo")
+                continue
+            try:
+                costo_servicio = float(costo_servicio_str)
+                if costo_servicio <= 0:
+                    print("El costo debe ser mayor a 0 pesos.")
+                    continue
+            except ValueError:
+                print("El cosot debe tener un número válido")
+                continue
+            break
+            
             costo_servicio = float(costo_servicio)
             
             if input("¿Agregar otro servicio? (s/n): ").lower() != "s":
                 break
             
         notas.append(nota)
+        print(f"\nFolio asignado: {folios_contador}")
+        print(f"Nombre: {nombre_cliente}")
+        print(f"Fecha: {fecha_ingresada.strftime('%d-%m-%Y')}")
+        print("Detalles: ")
+        for servicio, costo in nota['detalle']:
+            print(f"Servicio: {servicio} ----> Costo: {costo:.2f}")
+        print(f"Monto total a pagar: {nota['monto_a_pagar']:.2f}")
         print(f"Folio asignado: {folios_contador}")
     
     elif opcion == "2":
@@ -47,14 +91,47 @@ while True:
         consulta_opcion = input("Ingrese una opción de consulta:\n")
         
         if consulta_opcion == "1":
+            while True:
+                fecha_inicial_str = input("Fecha inicial (dd-mm-aaaa): ")
+                if not re.match(patron_fecha, fecha_inicial_str):
+                    print("Formato de fecha incorrecto. Debe ser dd-mm-aaaa: ")
+                    continue
+                try:
+                    fecha_final_str = input("Fecha final (dd-mm-aaaa): ")
+                except Exception:
+                    print("Formato de fecha incorrecto. Debe ser dd-mm-aaaa.")
+                    continue
+                try:
+                    fecha_inicial = datetime.datetime.strptime(fecha_inicial_str, "%d-%m-%Y")
+                except Exception:
+                    print("La fecha no es válida. Intente de nuevo")
+                    continue
+                break
+
+            while True:
+                fecha_final = datetime.datetime.strptime(fecha_final_str, "%d-%m-%Y")
+                if not re.match(patro_fecha, fecha_final_str):
+                    print("Formato de fecha incorrecto. Debe ser dd-mm-aaaa.")
+                    continue
+                try:
+                    fecha_final = datetime.datetime.strptime(fecha_final_str, "%d-%m-%Y")
+                except Exception:
+                    print("La fecha no es válida. Intente de nuevo.")
+                    continue
+                break
             
-            fecha_inicial_str = input("Fecha inicial (dd-mm-aaaa): ")
-            fecha_final_str = input("Fecha final (dd-mm-aaaa): ")
-            fecha_inicial = datetime.datetime.strptime(fecha_inicial_str, "%d-%m-%Y")
-            fecha_final = datetime.datetime.strptime(fecha_final_str, "%d-%m-%Y")
-            
-            notas_en_periodo = []
-            
+            notas_en_periodo = [nota for nota in notas if fecha_inicial <= nota['fecha'] <= fecha_final and nota['estado']]
+            if notas_en_periodo:
+                print("\nNotas encontradas en el periodo:")
+                print("-" * 50)
+                for nota in notas_en_periodo:
+                    print(f"\nFolio: {nota['folio']}\tCliente: {nota['Cliente'].strftime('%d-%m-%Y')}")
+                    print(f"Monto: {nota['monto_a_pagar']:.2f}")
+            else:
+                print("Fin de las notas")
+            else:
+                print("\nNo se encontraron notas en el periodo especificado.")
+                
             for nota in notas:
                 if nota['fecha']>=fecha_inicial and nota['fecha']<= fecha_final and nota['estado']:
                     notas_en_periodo.append(nota)
